0369ee483b5a3a2bb3e3add8f38ed59a3c7fc395
Michiel ter Reehorst <michiel@swydo.com>
2017-09-12 12:49:43 +0200

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,101 @@
-# node-cheddar-api
-Node.js Cheddar API wrapper
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+**Table of Contents**
+
+- [Goals and Status](#goals-and-status)
+- [Installation](#installation)
+- [Basic usage](#basic-usage)
+- [Using a proxy](#using-a-proxy)
+- [Running tests](#running-tests)
+- [Credits](#credits)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+# Goals and Status
+
+This module will simplify the process of integrating [Cheddar](https://www.getcheddar.com/) into your existing node.js apps.
+
+* `getAllPricingPlans()`
+* `getPricingPlan(planCode)`
+* `getAllCustomers([searchParams])`
+* `getCustomer(customerCode)`
+* `createCustomer(customerData)`
+* `editCustomerAndSubscription(customerData)` or `updateCustomerAndSubscription(customerData)`
+* `editCustomer(customerCode, customerData)` or `updateCustomer(customerCode, customerData)`
+* `editSubscription(customerCode, customerData)` or `updateSubscription(customerCode, customerData)`
+* `deleteCustomer(customerCode)`
+* `cancelSubscription(customerCode)`
+* `addItem(customerCode, itemCode, [amount])`
+* `removeItem(customerCode, itemCode, [amount])`
+* `setItemQuantity(customerCode, itemCode, amount)`
+* `addCustomCharge(customerCode, chargeCode, quantity, amount, description)`
+* `deleteCustomCharge(customerCode, chargeId)`
+* `resendInvoiceEmail(idOrNumber)`
+* `oneTimeInvoice(customerCode, {data})`
+
+All methods return a promise with the requested data in JSON format.
+
+# Installation
+
+```
+npm install cheddar
+```
+
+Also install the peer dependencies:
+
+```
+npm install request request-promise-native
+```
+
+# Basic usage
+
+```javascript
+var Cheddar = require("cheddar");
+
+var cheddar = new Cheddar("email@example.com", "passwordExample", "ProductCode");
+
+cheddar.getAllPricingPlans().then(function (results) {
+  console.log(results);
+}).catch(function (err) {
+  console.error(err);
+});
+```
+
+# Using a proxy
+
+You might want to set up a proxy to communicate with Cheddar. Cheddar blocks most of the Heroku servers (a range of AWS IPs):
+
+> Recently, a large block of IPs on the Heroku platform were listed by DenyHosts due to a spike in SSH brute force attacks coming from the Heroku platform. In short, you need to be coming from an IP that isn't listed.
+
+A proxy host is easily set with the `CHEDDAR_HOST` environment variable:
+
+```
+CHEDDAR_HOST=example.com node your-server.js
+```
+
+# Running tests
+First add a config file (`config.json`) with all your Cheddar credentials:
+
+```javascript
+{
+  "email": "EMAIL",
+  "pass": "PASSWORD",
+  "productCode": "PRODUCTCODE",
+  "planCode": "PLANCODE",
+  "itemCode": "ITEMCODE"
+}
+```
+
+Now you can install all dependencies and run the tests:
+
+```
+npm install
+npm test
+```
+
+> **WARNING:** Only run the tests on a development account to prevent any side effects in production
+
+> **NOTE**: Not all API calls have been fully tested yet.
+
+# Credits
+Original work was done by [Kevin Smith](https://github.com/respectTheCode).
---
+++ test/cheddar.spec.js
@@ -0,0 +1,188 @@
+/* eslint-disable func-names */
+/* eslint-env node, mocha */
+var chai = require('chai');
+var Cheddar = require('../lib/cheddar');
+var config = require('../config.json');
+
+async function wait(ms) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, ms);
+    });
+}
+
+describe('Cheddar', function () {
+    this.timeout(30000);
+    this.slow(2000);
+
+    beforeEach(function () {
+        this.customerCode1 = 'customerCode1';
+        this.customerCode2 = 'customerCode2';
+        this.cheddar = new Cheddar(config.email, config.pass, config.productCode);
+    });
+
+    describe('Plans', async function () {
+        describe('#getAllPricingPlans', function () {
+            it('should return a plan array', async function () {
+                var plans = await this.cheddar.getAllPricingPlans();
+
+                chai.expect(plans).to.be.an('array');
+                chai.expect(plans.length).to.be.at.least(1);
+            });
+        });
+
+        describe('#getPricingPlan', function () {
+            it('should return a single plan', async function () {
+                var plan = await this.cheddar.getPricingPlan(config.planCode);
+
+                chai.expect(typeof plan).to.equal('object');
+            });
+
+            it('should fail on bad plan code', async function () {
+                try {
+                    await this.cheddar.getPricingPlan('Bad Plan Code');
+                } catch (err) {
+                    chai.expect(err.message).to.include('Plan not found');
+                }
+            });
+        });
+    });
+
+    describe('Customers', function () {
+        describe('#createCustomer', function () {
+            it('should create a customer', async function () {
+                var subscriptionData = {
+                    planCode: config.planCode,
+                    method: 'cc',
+                    ccNumber: '4111111111111111',
+                    ccExpiration: '12/2020',
+                    ccCardCode: '123',
+                    ccFirstName: 'FName',
+                    ccLastName: 'LName',
+                    ccZip: '95123',
+                };
+
+                await this.cheddar.createCustomer({
+                    code: this.customerCode1,
+                    firstName: 'FName',
+                    lastName: 'LName',
+                    email: 'test@example.com',
+                    subscription: subscriptionData,
+                });
+
+                // Make sure the createdDatetime are different for each user
+                await wait(2000);
+
+                await this.cheddar.createCustomer({
+                    code: this.customerCode2,
+                    firstName: 'FName2',
+                    lastName: 'LName2',
+                    email: 'test2@example.com',
+                    subscription: subscriptionData,
+                });
+
+                // Make the customer is saved before continuing
+                await wait(2000);
+            });
+        });
+
+        describe('#getAllCustomers', function () {
+            it('should retrieve all customers', async function () {
+                var options = {
+                    planCode: [config.planCode],
+                    subscriptionStatus: 'activeOnly',
+                    orderBy: 'createdDatetime',
+                    orderByDirection: 'desc',
+                    createdAfterDate: '2017-01-01',
+                };
+
+                var customers = await this.cheddar.getAllCustomers(options);
+
+                chai.expect(customers).to.be.an('array');
+                chai.expect(customers.length).to.equal(2);
+                chai.expect(customers[0].code).to.equal(this.customerCode2);
+            });
+        });
+
+        describe('#getCustomer', function () {
+            it('should retrieve a customer with the right code', async function () {
+                var customer = await this.cheddar.getCustomer(this.customerCode1);
+                chai.expect(customer).to.be.an('object');
+            });
+
+            it('should fail with bad code', async function () {
+                try {
+                    await this.cheddar.getCustomer('Bad Customer Code');
+                } catch (err) {
+                    chai.expect(err.message).to.include('Customer not found');
+                }
+            });
+        });
+    });
+
+    describe('Items', function () {
+        describe('#setItemQuantity', function () {
+            it('should increase the item count', async function () {
+                await this.cheddar.setItemQuantity(this.customerCode1, config.itemCode, 1);
+
+                var customer = await this.cheddar.getCustomer(this.customerCode1);
+
+                chai.expect(customer.subscriptions[0].items[0].quantity).to.equal(1);
+            });
+        });
+
+        describe('#addItem', function () {
+            it('should add to the item count', async function () {
+                await this.cheddar.addItem(this.customerCode1, config.itemCode, 2);
+
+                var customer = await this.cheddar.getCustomer(this.customerCode1);
+
+                chai.expect(customer.subscriptions[0].items[0].quantity).to.equal(1 + 2);
+            });
+
+            it('should default to 1 as item count', async function () {
+                await this.cheddar.addItem(this.customerCode1, config.itemCode);
+
+                var customer = await this.cheddar.getCustomer(this.customerCode1);
+
+                chai.expect(customer.subscriptions[0].items[0].quantity).to.equal(1 + 2 + 1);
+            });
+        });
+
+        describe('#removeItem', function () {
+            it('should decrease the item count', async function () {
+                await this.cheddar.removeItem(this.customerCode1, config.itemCode, 2);
+
+                var customer = await this.cheddar.getCustomer(this.customerCode1);
+
+                chai.expect(customer.subscriptions[0].items[0].quantity).to.equal(2);
+            });
+
+            it('should default to 1 as item count', async function () {
+                await this.cheddar.removeItem(this.customerCode1, config.itemCode);
+
+                var customer = await this.cheddar.getCustomer(this.customerCode1);
+
+                chai.expect(customer.subscriptions[0].items[0].quantity).to.equal(1);
+            });
+        });
+    });
+
+    describe('#deleteCustomer', function () {
+        it('should remove a specific customer', async function () {
+            await this.cheddar.deleteCustomer(this.customerCode1);
+        });
+    });
+
+    describe('#deleteAllCustomers', function () {
+        it('should remove all customers (in development mode)', async function () {
+            var ts = Math.round((new Date()).getTime() / 1000) + 2000;
+            await this.cheddar.deleteAllCustomers(ts);
+
+            try {
+                this.cheddar.getAllCustomers({});
+            } catch (err) {
+                chai.expect(err.message).to.include('No customers found');
+            }
+        });
+    });
+});
