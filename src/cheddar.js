7a8dec651c8ea6939cd3a53873d0a65d4776221b
Michiel ter Reehorst <michiel@swydo.com>
2017-09-27 16:56:30 +0200

    Move all functions to class

--- src/cheddar.js
+++ src/cheddar.js
@@ -3,21 +3,6 @@ const rp = require('request-promise-native');
 
 const baseUri = 'https://getcheddar.com:443';
 
-class Cheddar {
-    constructor(user, pass, productCode) {
-        if (typeof user === 'string') {
-            this.auth = 'Basic ' + Buffer.from(user + ':' + pass).toString('base64');
-            this.productCode = productCode;
-        } else {
-            const options = user;
-
-            this.auth = 'Basic ' + Buffer.from(options.username + ':' + options.password).toString('base64');
-            this.productCode = options.productCode;
-            this.productId = options.productId;
-        }
-    }
-}
-
 const arrays = [
     'plans',
     'customers',
@@ -87,181 +72,196 @@ function parseResult(data) {
     });
 }
 
-Cheddar.prototype.callAPI = function callAPI(path, data) {
-    // Encode the path, because some codes can contain spaces
-    const encodedPath = encodeURI(path);
+class Cheddar {
+    constructor(user, pass, productCode) {
+        if (typeof user === 'string') {
+            this.auth = 'Basic ' + Buffer.from(user + ':' + pass).toString('base64');
+            this.productCode = productCode;
+        } else {
+            const options = user;
 
-    const requestOptions = {
-        uri: baseUri + '/xml' + encodedPath,
-        headers: {
-            authorization: this.auth,
-        },
-        form: data,
-        proxy: process.env.CHEDDAR_PROXY_URL,
-        qs: {
-            productCode: this.productCode,
-            productId: this.productId,
-        },
-    };
+            this.auth = 'Basic ' + Buffer.from(options.username + ':' + options.password).toString('base64');
+            this.productCode = options.productCode;
+            this.productId = options.productId;
+        }
+    }
 
-    const promise = rp.post(requestOptions)
-        .then(parseResult)
-        .catch(function handleError(err) {
-            if (typeof err.error === 'string' && err.error.indexOf('<?xml') === 0) {
-                return parseResult(err.error).then(function createErrorFromXml(xml) {
-                    const error = new Error(xml.error._);
-                    error.code = Number(xml.error.code);
-                    throw error;
-                });
-            }
-            throw err;
-        });
+    callAPI(path, data = {}) {
+        // Encode the path, because some codes can contain spaces
+        const encodedPath = encodeURI(path);
 
-    return promise;
-};
+        const requestOptions = {
+            uri: baseUri + '/xml' + encodedPath,
+            headers: {
+                authorization: this.auth,
+            },
+            form: data,
+            proxy: process.env.CHEDDAR_PROXY_URL,
+            qs: {
+                productCode: this.productCode,
+                productId: this.productId,
+            },
+        };
 
-Cheddar.prototype.callReportingAPI = function callReportingAPI(path, data) {
-    // Encode the path, because some codes can contain spaces
-    const encodedPath = encodeURI(path);
+        const promise = rp.post(requestOptions)
+            .then(parseResult)
+            .catch(function handleError(err) {
+                if (typeof err.error === 'string' && err.error.indexOf('<?xml') === 0) {
+                    return parseResult(err.error).then(function createErrorFromXml(xml) {
+                        const error = new Error(xml.error._);
+                        error.code = Number(xml.error.code);
+                        throw error;
+                    });
+                }
+                throw err;
+            });
 
-    const requestOptions = {
-        uri: baseUri + '/json' + encodedPath,
-        headers: {
-            authorization: this.auth,
-        },
-        qs: {
-            productCode: this.productCode,
-            productId: this.productId,
-            startDate: data.dateRange && data.dateRange.start,
-            endDate: data.dateRange && data.dateRange.end,
-        },
-        proxy: process.env.CHEDDAR_PROXY_URL,
-        json: true,
-    };
+        return promise;
+    }
 
-    return rp.get(requestOptions);
-};
+    callReportingAPI(path, data = {}) {
+        // Encode the path, because some codes can contain spaces
+        const encodedPath = encodeURI(path);
 
-Cheddar.prototype.getAllPricingPlans = function getAllPricingPlans() {
-    return this.callAPI('/plans/get');
-};
+        const requestOptions = {
+            uri: baseUri + '/json' + encodedPath,
+            headers: {
+                authorization: this.auth,
+            },
+            qs: {
+                productCode: this.productCode,
+                productId: this.productId,
+                startDate: data.dateRange && data.dateRange.start,
+                endDate: data.dateRange && data.dateRange.end,
+            },
+            proxy: process.env.CHEDDAR_PROXY_URL,
+            json: true,
+        };
 
-Cheddar.prototype.getPricingPlan = function getPricingPlan(code) {
-    const promise = this.callAPI('/plans/get/code/' + code)
-        .then(function getFirstPlan(plans) {
-            // Return the first plan (it should only contain 1)
-            return plans && plans[0];
-        });
+        return rp.get(requestOptions);
+    }
 
-    return promise;
-};
+    getAllPricingPlans() {
+        return this.callAPI('/plans/get');
+    }
 
-Cheddar.prototype.getAllCustomers = function getAllCustomers(data) {
-    return this.callAPI('/customers/get', data);
-};
+    getPricingPlan(code) {
+        const promise = this.callAPI('/plans/get/code/' + code)
+            .then(function getFirstPlan(plans) {
+                // Return the first plan (it should only contain 1)
+                return plans && plans[0];
+            });
 
-Cheddar.prototype.getCustomer = function getCustomer(code) {
-    const promise = this.callAPI('/customers/get/code/' + code)
-        .then(function getFirstCustomer(customers) {
-            if (!customers || !customers.length) {
-                throw new Error('No customers could be retrieved');
-            }
-            // Return the first customer (it should only contain 1)
-            return customers[0];
-        });
+        return promise;
+    }
 
-    return promise;
-};
+    getAllCustomers(data) {
+        return this.callAPI('/customers/get', data);
+    }
 
-Cheddar.prototype.createCustomer = function createCustomer(data) {
-    return this.callAPI('/customers/new', data);
-};
+    getCustomer(code) {
+        const promise = this.callAPI('/customers/get/code/' + code)
+            .then(function getFirstCustomer(customers) {
+                if (!customers || !customers.length) {
+                    throw new Error('No customers could be retrieved');
+                }
+                // Return the first customer (it should only contain 1)
+                return customers[0];
+            });
 
-Cheddar.prototype.editCustomerAndSubscription = function editCustomerAndSubscription(code, data) {
-    return this.callAPI('/customers/edit/code/' + code, data);
-};
+        return promise;
+    }
+
+    createCustomer(data) {
+        return this.callAPI('/customers/new', data);
+    }
+
+    editCustomerAndSubscription(code, data) {
+        return this.callAPI('/customers/edit/code/' + code, data);
+    }
+
+    editCustomer(code, data) {
+        return this.callAPI('/customers/edit-customer/code/' + code, data);
+    }
+
+    editSubscription(code, data) {
+        return this.callAPI('/customers/edit-subscription/code/' + code, data);
+    }
+
+    deleteCustomer(code) {
+        return this.callAPI('/customers/delete/code/' + code);
+    }
+
+    deleteAllCustomers(unixtimestamp) {
+        return this.callAPI('/customers/delete-all/confirm/' + unixtimestamp + '');
+    }
+
+    cancelSubscription(code) {
+        return this.callAPI('/customers/cancel/code/' + code);
+    }
+
+    addItem(code, itemCode, amount) {
+        let data;
+
+        if (amount) {
+            data = { quantity: amount.toString() };
+        }
+
+        return this.callAPI('/customers/add-item-quantity/code/' + code + '/itemCode/' + itemCode, data);
+    }
+
+    removeItem(code, itemCode, amount) {
+        let data;
+
+        if (amount) {
+            data = { quantity: amount.toString() };
+        }
+
+        return this.callAPI('/customers/remove-item-quantity/code/' + code + '/itemCode/' + itemCode, data);
+    }
+
+    setItemQuantity(code, itemCode, amount) {
+        const data = { quantity: amount.toString() };
+        return this.callAPI('/customers/set-item-quantity/code/' + code + '/itemCode/' + itemCode, data);
+    }
+
+    addCustomCharge(code, chargeCode, quantity, amount, description) {
+        const data = {
+            chargeCode: chargeCode,
+            quantity: quantity.toString(),
+            eachAmount: amount.toString(),
+            description: description,
+        };
+
+        return this.callAPI('/customers/add-charge/code/' + code, data);
+    }
+
+    deleteCustomCharge(code, chargeId) {
+        const data = {
+            chargeId: chargeId,
+        };
+        return this.callAPI('/customers/delete-charge/code/' + code, data);
+    }
+
+    resendInvoiceEmail(idOrNumber) {
+        let data;
+
+        if (isNaN(idOrNumber)) {
+            data = { id: idOrNumber };
+        } else {
+            data = { number: idOrNumber };
+        }
+
+        return this.callAPI('/invoices/send-email', data);
+    }
+
+    oneTimeInvoice(customerCode, data) {
+        return this.callAPI('/invoices/new/code/' + customerCode, data);
+    }
+}
+
 Cheddar.prototype.updateCustomerAndSubscription = Cheddar.prototype.editCustomerAndSubscription;
-
-Cheddar.prototype.editCustomer = function editCustomer(code, data) {
-    return this.callAPI('/customers/edit-customer/code/' + code, data);
-};
 Cheddar.prototype.updateCustomer = Cheddar.prototype.editCustomer;
-
-Cheddar.prototype.editSubscription = function editSubscription(code, data) {
-    return this.callAPI('/customers/edit-subscription/code/' + code, data);
-};
 Cheddar.prototype.updateSubscription = Cheddar.prototype.editSubscription;
-
-Cheddar.prototype.deleteCustomer = function deleteCustomer(code) {
-    return this.callAPI('/customers/delete/code/' + code);
-};
-
-Cheddar.prototype.deleteAllCustomers = function deleteAllCustomers(unixtimestamp) {
-    return this.callAPI('/customers/delete-all/confirm/' + unixtimestamp + '');
-};
-
-Cheddar.prototype.cancelSubscription = function cancelSubscription(code) {
-    return this.callAPI('/customers/cancel/code/' + code);
-};
-
-Cheddar.prototype.addItem = function addItem(code, itemCode, amount) {
-    let data;
-
-    if (amount) {
-        data = { quantity: amount.toString() };
-    }
-
-    return this.callAPI('/customers/add-item-quantity/code/' + code + '/itemCode/' + itemCode, data);
-};
-
-Cheddar.prototype.removeItem = function removeItem(code, itemCode, amount) {
-    let data;
-
-    if (amount) {
-        data = { quantity: amount.toString() };
-    }
-
-    return this.callAPI('/customers/remove-item-quantity/code/' + code + '/itemCode/' + itemCode, data);
-};
-
-Cheddar.prototype.setItemQuantity = function setItemQuantity(code, itemCode, amount) {
-    const data = { quantity: amount.toString() };
-    return this.callAPI('/customers/set-item-quantity/code/' + code + '/itemCode/' + itemCode, data);
-};
-
-Cheddar.prototype.addCustomCharge =
-function addCustomCharge(code, chargeCode, quantity, amount, description) {
-    const data = {
-        chargeCode: chargeCode,
-        quantity: quantity.toString(),
-        eachAmount: amount.toString(),
-        description: description,
-    };
-
-    return this.callAPI('/customers/add-charge/code/' + code, data);
-};
-
-Cheddar.prototype.deleteCustomCharge = function deleteCustomCharge(code, chargeId) {
-    const data = {
-        chargeId: chargeId,
-    };
-    return this.callAPI('/customers/delete-charge/code/' + code, data);
-};
-
-Cheddar.prototype.resendInvoiceEmail = function resendInvoiceEmail(idOrNumber) {
-    let data;
-
-    if (isNaN(idOrNumber)) {
-        data = { id: idOrNumber };
-    } else {
-        data = { number: idOrNumber };
-    }
-
-    return this.callAPI('/invoices/send-email', data);
-};
-
-Cheddar.prototype.oneTimeInvoice = function oneTimeInvoice(customerCode, data) {
-    return this.callAPI('/invoices/new/code/' + customerCode, data);
-};
 
 module.exports = Cheddar;
