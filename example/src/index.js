b8add9b22dd56a8ebfbeaec0805a1ad1eb66ae0a
Victor Uvarov <victoruvarov23@gmail.com>
2025-05-19 19:29:06 -0700

    fix esm and cjs exports

---
+++ example/src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var cheddar_api_1 = require("@tivetechnology/cheddar-api");
+var cheddar = new cheddar_api_1.Cheddar({
+    username: "username",
+    password: "password",
+    productCode: "code",
+});
+console.log(cheddar);
