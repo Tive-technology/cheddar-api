7c18c83d30515347cf421998760c99211ce87c7b
Victor Uvarov <victoruvarov23@gmail.com>
2025-05-19 19:29:57 -0700

    .

--- example/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cheddar_api_1 = require("@tivetechnology/cheddar-api");
-var cheddar = new cheddar_api_1.Cheddar({
-    username: "username",
-    password: "password",
-    productCode: "code",
-});
-console.log(cheddar);
